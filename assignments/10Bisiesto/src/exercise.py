b61a75db0edf710cfc8fa71a5a3365c582555c7a
RobertoECC <88851027+RobertoECC@users.noreply.github.com>
2021-08-27 14:38:45 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,15 @@
+def bis(a):
+    bisiesto = (a%4)
+    return(bisiesto)
 
 def main():
     #escribe tu código abajo de esta línea
     pass
+
+año = int(input(""))
+if bis(año)==0:
+    print("True")
+else: print("False")
 
 if __name__=='__main__':
     main()
