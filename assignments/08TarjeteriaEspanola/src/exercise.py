49b48d504b8d19e4c9acf8f6c23999e89992147a
RobertoECC <88851027+RobertoECC@users.noreply.github.com>
2021-08-27 19:58:02 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,7 +1,23 @@
+def papel(p):
+    tarjetas = p*12
+    return (tarjetas)
+
+def plumones(pm):
+    tarjetas = pm*35
+    return (tarjetas)
 
 def main():
+    
     #escribe tu código abajo de esta línea
     pass
+
+pliegos = int(input("Dame la cantidad de pliegos de papel albanene: "))
+plumn = int(input("Dame la cantidad de plumones: "))
+
+if papel(pliegos) > plumones(plumn):
+    print("El número máximo de tarjetas que se pueden hacer es: ", plumones(plumn))
+elif papel(pliegos) < plumones(plumn):
+    print("El número máximo de tarjetas que se pueden hacer es: ", papel(pliegos))
 
 if __name__=='__main__':
     main()
